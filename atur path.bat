807ea66f8c2bb941f69967b96a946b332747c79c
fayoverlay <achmadripai.skom@gmail.com>
2024-09-16 04:10:34 +0700

    auto commit

--- atur path.bat
+++ atur path.bat
@@ -2,6 +2,8 @@
 set gitdir=c:\portablegit
 set path=%gitdir%\cmd;%path%
 pause
+git config user.email "achmadripai.skom@gmail.com"
+git config user.name "fayoverlay"
 git add .
 git commit -m "auto commit"
 git push origin main
